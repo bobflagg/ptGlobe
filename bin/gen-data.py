ffd136859ea1e27e056eacfaa480ef45035a131e
Bob Flagg <bob@calcworks.net>
2012-11-24 14:20:36 -0500

    Updated data generator to produce bogus research sites for mock up.

--- bin/gen-data.py
+++ bin/gen-data.py
@@ -1,12 +1,19 @@
 '''
 Utility module for dumping project data to a json file suitable for use
 in geo.json apps.
+
+import ConfigParser
+from geo import project_data as pd
+config = ConfigParser.ConfigParser()
+config.read("config/truonex.cfg")
+pd.dump_to_json(config)
 '''
 import ConfigParser
 import GeoIP
 import json
 import socket
 from util.db import DataManager
+from random import random
 
 TASK = [
     'dump_to_json',        # 0
@@ -21,12 +28,14 @@ def main():
     globals()[TASK](config)
 
 def dump_to_json(config):
+    data_dir = "/home/birksworks/Dev/ptGlobe/www/data"
     counts = {}
     locations = {}
     data = {
         "type":"FeatureCollection",
         "features":[]
     }
+    city_data = {}
     gi = GeoIP.open("/usr/share/GeoIP/GeoLiteCity.dat",GeoIP.GEOIP_STANDARD)
     dbm = DataManager(config)  
     for project in dbm.get_project_data():
@@ -39,10 +48,17 @@ def dump_to_json(config):
                 region = info['region']
                 country = info['country_code']
                 key = "%s::%s::%s" % (city, region, country)
-                if key in counts: counts[key] = counts[key] + 1
+                if key in counts: 
+                    counts[key] = counts[key] + 1
                 else: 
                     counts[key] = 1
                     locations[key] = {"type":"Point","coordinates":[info['longitude'], info['latitude']]}
+                    city_data[key] = {}                       
+                city_data[key][project['id']] = [
+                    info['longitude'] + (2.0 * round(random(),2) - 1.0) / 10.0, 
+                    info['latitude'] + round((2.0 * random() - 1.0), 1) / 10.0, 
+                    project['title']
+                ]
             except:
                 print url
     for key in counts.keys():
@@ -54,8 +70,27 @@ def dump_to_json(config):
             'id':key
         }
         data['features'].append(feature)
-    fp = open('/var/www/geo-proj/data/projects.json', 'w')
+        longitude = locations[key]["coordinates"][0]
+        latitude = locations[key]["coordinates"][1]
+        if counts[key] <= 10:
+            n_sites = counts[key]
+        elif counts[key] <= 40:
+            n_sites = counts[key] / 2
+        else:
+            n_sites = 20
+        site_data = {}
+        for i in range(n_sites):
+            site_data["%d" % (i + 1)] = [
+                longitude + (2.0 * round(random(),2) - 1.0) / 10.0, 
+                latitude + round((2.0 * random() - 1.0), 1) / 10.0, 
+                "%s Research Site %d" % (city, i + 1)
+            ]
+        fp = open('%s/%s.json' % (data_dir, key.replace('::', '-')), 'w')
+        fp.write(json.dumps(site_data))
+        fp.close()
+    fp = open('%s/cities.json' % data_dir, 'w')
     fp.write(json.dumps(data))
+    fp.close()
      
 if __name__ == "__main__":
     main()    
